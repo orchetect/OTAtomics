57b9a8802183e71ab1d9e9cef67696cf95dba62d
Steffan Andrews <stef@orchetect.com>
2024-11-24 23:30:54 -0800

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -4,31 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "OTAtomics",
-    
     platforms: [
         .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
-    
     products: [
-        .library(
-            name: "OTAtomics",
-            type: .static,
-            targets: ["OTAtomics"]
-        )
+        .library(name: "OTAtomics", type: .static, targets: ["OTAtomics"])
     ],
-    
-    dependencies: [
-        // none
-    ],
-    
     targets: [
-        .target(
-            name: "OTAtomics",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "OTAtomicsTests",
-            dependencies: ["OTAtomics"]
-        )
+        .target(name: "OTAtomics"),
+        .testTarget(name: "OTAtomicsTests", dependencies: ["OTAtomics"])
     ]
 )
