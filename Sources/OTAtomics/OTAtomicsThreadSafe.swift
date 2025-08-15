2d0255448bd909e7e4d6f61766333cfeb4aa2e15
Steffan Andrews <stef@orchetect.com>
2025-08-14 18:42:52 -0700

    Formatting

--- Sources/OTAtomics/OTAtomicsThreadSafe.swift
+++ Sources/OTAtomics/OTAtomicsThreadSafe.swift
@@ -9,7 +9,8 @@ import Darwin
 /// `OTAtomicsThreadSafe`: A property wrapper that ensures thread-safe atomic access to a value.
 /// Multiple read accesses can potentially read at the same time, just not during a write.
 ///
-/// By using `pthread` to do the locking, this safer than using a `DispatchQueue/barrier` as there isn't a chance of priority inversion.
+/// By using `pthread` to do the locking, this safer than using a `DispatchQueue/barrier` as there
+/// isn't a chance of priority inversion. It is also generally more performant than queue sync or `NSLock`.
 ///
 /// This is safe to use on collection types (`Array`, `Dictionary`, etc.)
 ///
